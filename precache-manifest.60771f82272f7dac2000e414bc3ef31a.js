2b97fc1fe0f2ac5aac430aeee63889eccb2c91b4
Achyuth17 <51409439+Achyuth17@users.noreply.github.com>
2020-10-20 11:49:57 +0530

    Updates

---
+++ precache-manifest.60771f82272f7dac2000e414bc3ef31a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a385d27ad7d7ee42ea105281dbda10",
+    "url": "/React_TodoList/index.html"
+  },
+  {
+    "revision": "7300477b79ccd7678436",
+    "url": "/React_TodoList/static/css/main.ddfee3b2.chunk.css"
+  },
+  {
+    "revision": "9adce8b3d149d67b33c2",
+    "url": "/React_TodoList/static/js/2.1a901f02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_TodoList/static/js/2.1a901f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7300477b79ccd7678436",
+    "url": "/React_TodoList/static/js/main.73d5626d.chunk.js"
+  },
+  {
+    "revision": "07d2194a28fae400c957",
+    "url": "/React_TodoList/static/js/runtime-main.389116c3.js"
+  }
+]);
